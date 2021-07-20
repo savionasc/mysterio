fdd33f45c5f3bb8a8d8af53ef66c338281cdd69c
Savio <savionasc@gmail.com>
2021-07-20 19:01:31 -0300

    Nova organização do Command

--- src/mission/Command.h
+++ src/mission/Command.h
@@ -1,10 +1,15 @@
 #ifndef MYSTERIO_SRC_MISSION_COMMAND_H_
 #define MYSTERIO_SRC_MISSION_COMMAND_H_
+#include <iostream>
+#include <vector>
+using namespace std;
 
 class Command {
 public:
-    Command(){}
+    Command(){string cmd = "go"; this->v.push_back(cmd);} //comand sendMessage
     virtual ~Command(){}
+private:
+    vector<string> v;
 };
 
 #endif
