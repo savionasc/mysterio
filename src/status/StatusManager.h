0f0cd507c6fd81b8e6f3b04d65ed7aac39cfaf0b
Savio <savionasc@gmail.com>
2022-01-17 13:54:31 -0300

    Removendo linhas desnecessárias

--- src/status/StatusManager.h
+++ src/status/StatusManager.h
@@ -14,15 +14,6 @@ public:
     virtual int getFlightTime(int idUAV) = 0;
     virtual int CountActiveUAVs() = 0;
     virtual float getBattery(int idUAV) = 0;
-
-    /*ReadStatusFromDB(int idUAV){
-     * Repository r;
-     * return r.getStatusLocation(idUAV);
-     *}*/
-
-    //virtual void subscribe(UAV uav) = 0;
-    //virtual std::list<UAV*> getPublishersList() = 0;
-    //virtual void unsubscribe(UAV uav) = 0;
 };
 
 #endif
