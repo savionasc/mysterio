5f4a99aeb6d78cc4cca629d43443e1f2803c2c58
Savio <savionasc@gmail.com>
2022-03-26 12:58:41 -0300

    R. Comentários desnecessários

--- src/utils/UAV.h
+++ src/utils/UAV.h
@@ -6,12 +6,7 @@
 #include "HardwareInformation.h"
 #include "NetworkConfigurations.h"
 
-//#include "../communication/Communicable.h"
-
-//Talvez extender/implementar communicable
 class UAV{
-    //: public Communicable { //Aqui eu pensei que seria um exemplo simples duma instância dum uav, tipo isso
-    // MOVER PARA UTILS E MUDAR PARA UAV
 public:
     UAV();
     UAV(int idUAV);
@@ -20,24 +15,6 @@ public:
     virtual void setID(int id);
     //virtual int getIdSocket();
     //virtual void setIdSocket(int id);
-    //virtual double getXAxis();
-    //virtual void setXAxis(double xAxis);
-    //virtual double getYAxis();
-    //virtual void setYAxis(double yAxis);
-    //virtual double getZAxis();
-    //virtual void setZAxis(double zAxis);
-    //virtual int getTempoDeVoo();
-    //virtual void setTempoDeVoo(int tempodevoo);
-    //virtual double getVelocidade();
-    //virtual void setVelocidade(double velocidade);
-
-    //Communicable
-    //void onMessageReceive(Message msg);
-
-    //virtual float getBattery();
-    //virtual void setBattery(float battery);
-    //virtual int getFlightTime();
-    //virtual void setFlightTime(int flightTime);
 
     UAVStatus getStatus() {
         return status;
@@ -66,13 +43,6 @@ public:
 protected:
     int idUAV;
     //int idSocket;
-    //int tempoDeVoo;
-    //double xAxis;
-    //double yAxis;
-    //double zAxis;
-    //double velocidade;
-    //float battery;
-    //int flightTime;
 
     Competence competence;
 
@@ -81,8 +51,7 @@ protected:
     HardwareInformation hardwareInformation;
 
     NetworkConfigurations networkConfigurations;
-    //bateria
-    //disponivel
+    //status de disponibilidade
 };
 
 #endif
