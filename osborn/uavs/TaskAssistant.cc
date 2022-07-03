8918f30f7eef9213affb8228d73135003d660f56
Sávio Nascimento <savionasc@gmail.com>
2022-07-03 15:23:52 -0300

    Coordenadas para Formação

--- osborn/uavs/TaskAssistant.cc
+++ osborn/uavs/TaskAssistant.cc
@@ -9,20 +9,18 @@ TaskAssistant::TaskAssistant() {}
 TaskAssistant::~TaskAssistant() {}
 
 std::vector<Coordinate> TaskAssistant::splitCoordinate(Coordinate coord, int slice){
-
-    /*int size = qtyDivisions(coord, 50);
-    Coordinate coords[size];
-    coordinatesToFollow(coord, coords, size);
-    for (int var = 0; var < size; ++var) {
-        v[uav]->push_back(coord[var]);
-        cout << "PRINT " << var << endl;
-        cout << "x: " << coords[var].getX() << " y: " << coords[var].getY() << " z: " << coords[var].getZ() << endl;
-    }*/
-
     vector<Coordinate> v;
     int size = qtyDivisions(coord, slice);
-    cout << "size: " << size << endl;
     coordinatesToFollowVector(coord, &v, size);
+
+    return v;
+}
+
+std::vector<Coordinate> TaskAssistant::splitCoordinateFormation(Coordinate coord, Coordinate target, int slice){
+    vector<Coordinate> v;
+    Coordinate diff = diference(target, coord);
+    int size = qtyDivisions(diff, slice);
+    coordinatesToFollowVectorFormation(coord, diff, &v, size);
 
     return v;
 }
@@ -30,14 +28,10 @@ std::vector<Coordinate> TaskAssistant::splitCoordinate(Coordinate coord, int sli
 int TaskAssistant::qtyDivisions(Coordinate coord, int divider){
     int x, y, z, quotient = 0;
 
-    cout << "qty x: " << coord.getX() << " y: " << coord.getY() << " z: " << coord.getZ() << endl;
-
     //quotient calculation
     x = coord.getX()/divider;
     y = coord.getY()/divider;
     z = coord.getZ()/divider;
-
-    cout << "divider x: " << x << " y: " << y << " z: " << z << endl;
 
     //returns the largest of the 3 quotient divisions
     if(x >= y && x >= z)
@@ -68,4 +62,22 @@ void TaskAssistant::coordinatesToFollowVector(Coordinate coord, std::vector<Coor
         Coordinate c(x, y, z);
         v->push_back(c);
     }
+}
+
+void TaskAssistant::coordinatesToFollowVectorFormation(Coordinate coord, Coordinate diff, std::vector<Coordinate> *v, int length){
+    for (int i = 0; i < length; i++) {
+        float x, y, z;
+        x = (floor((diff.getX())) / length ) * (i+1);
+        y = floor( (diff.getY() / length) * (i+1) );
+        z = floor( (diff.getZ() / length) * (i+1) );
+        Coordinate c(coord.getX()+x, coord.getY()+y, coord.getZ()+z);
+        v->push_back(c);
+    }
+}
+
+Coordinate TaskAssistant::diference(Coordinate coord1, Coordinate coord2){
+    return Coordinate(
+            coord1.getX()-coord2.getX(),
+            coord1.getY()-coord2.getY(),
+            coord1.getZ()-coord2.getZ());
 }
--- osborn/uavs/TaskAssistant.h
+++ osborn/uavs/TaskAssistant.h
@@ -10,13 +10,15 @@ class TaskAssistant {
 public:
     TaskAssistant();
     virtual ~TaskAssistant();
-    //Coordinate* splitCoordinate(Coordinate coord);
     std::vector<Coordinate> splitCoordinate(Coordinate coord, int slice);
+    std::vector<Coordinate> splitCoordinateFormation(Coordinate coord, Coordinate target, int slice);
 
 private:
     int qtyDivisions(Coordinate c, int divider);
     void coordinatesToFollow(Coordinate coord, Coordinate *coords, int length);
     void coordinatesToFollowVector(Coordinate coord, std::vector<Coordinate> *v, int length);
+    void coordinatesToFollowVectorFormation(Coordinate coord, Coordinate diff, std::vector<Coordinate> *v, int length);
+    Coordinate diference(Coordinate coord1, Coordinate coord2);
 };
 
-#endif /* MYSTERIO_OSBORN_UAVS_TASKASSISTANT_H_ */
+#endif
