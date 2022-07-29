111b9b5d6313947e2d9681c130c3e44266766236
Sávio Nascimento <savionasc@gmail.com>
2022-07-28 21:55:12 -0300

    Updating communication

--- src/communication/Communication.h
+++ src/communication/Communication.h
@@ -10,9 +10,9 @@
 // Componente de comunicação do framework com Classes do tipo Communicable
 class Communication {
 public:
-    //Antigo sendMessage
+    virtual bool connectUAV(UAV u) = 0;
+
     virtual void ReceiveMessageFromUAV(Communicable *source, Communicable *dest, Message msg) = 0;
-    //virtual int* getActiveConnections(); retorna lista ou map de conexoes...
 
     virtual int countActiveConnections() = 0;
 
@@ -20,11 +20,9 @@ public:
 
     virtual int configureConnection(int connection) = 0;
 
-    virtual int configureSocketServer(int portServer){ return true; }
-
     virtual void sendMessageToUAV(int idUAV, Message msg) = 0;
 
-    virtual void reconnect(UAV u) = 0;
+    virtual void reconnectUAV(UAV u) = 0;
 
     //virtual void setConfigure(Config configures) = 0;
     //Aqui criaria uma classe Config no utils que talvez fosse importantes para guardar IPServer, PORT...
